5ee148c513960f65862117e876058d5358113ba2
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-03-26 15:51:38 +0530

    os Module

---
+++ osModule/index.js
@@ -0,0 +1,17 @@
+const os = require('os')
+// console.log(os);
+
+console.log(os.platform());
+console.log(os.totalmem());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.arch());
+console.log(os.networkInterfaces());
+
+console.log(os.userInfo());
+console.log(os.uptime());
+console.log(os.hostname());
+console.log(os.release());
+
+
